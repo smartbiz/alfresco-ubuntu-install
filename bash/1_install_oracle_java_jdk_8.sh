86c158740ae84e6a74ab9d7d820431401bb26b8c
smartbiz <sun.smartbiz@gmail.com>
2017-05-10 13:13:00 +0700

    1_install_oracle_java_jdk_8.sh

--- bash/1_install_oracle_java_jdk_8.sh
+++ bash/1_install_oracle_java_jdk_8.sh
@@ -1,6 +1,31 @@
 export DEFAULTYESNO="y"
 export APTVERBOSITY="-qq -y"
 
+
+# Color variables
+txtund=$(tput sgr 0 1)          # Underline
+txtbld=$(tput bold)             # Bold
+bldred=${txtbld}$(tput setaf 1) #  red
+bldgre=${txtbld}$(tput setaf 2) #  red
+bldblu=${txtbld}$(tput setaf 4) #  blue
+bldwht=${txtbld}$(tput setaf 7) #  white
+txtrst=$(tput sgr0)             # Reset
+info=${bldwht}*${txtrst}        # Feedback
+pass=${bldblu}*${txtrst}
+warn=${bldred}*${txtrst}
+ques=${bldblu}?${txtrst}
+
+echoblue () {
+  echo "${bldblu}$1${txtrst}"
+}
+echored () {
+  echo "${bldred}$1${txtrst}"
+}
+echogreen () {
+  echo "${bldgre}$1${txtrst}"
+}
+
+
 echo
 echoblue "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
 echo "Install Java JDK."
